c1164a3f603a9c8bd2435e230019b1912c1e607f
thepinkturtle <devinlax@hotmail.com>
2019-09-06 13:30:27 -0700

    change the underscore, mins, pipe, plus, L/R curly_brackets and

--- keymap.c
+++ keymap.c
@@ -57,11 +57,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT( \
   //,-----------------------------------------.                ,-----------------------------------------.
-     KC_TILD, KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,              KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_BSPC,\
+     KC_TILD, KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,            KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_BSPC,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_UNDS,KC_EQL,KC_LCBR,KC_RCBR,KC_PIPE,KC_GRV,\
+    KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_UNDS,KC_MINS,KC_EQL,KC_LCBR,KC_RCBR,KC_NO,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_MINS,KC_PLUS,KC_LBRC,KC_RBRC,KC_BSLS, KC_NO,\
+    KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_PIPE,KC_PLUS,KC_GRV,KC_LBRC,KC_RBRC,KC_BSLS,\
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
                                 KC_LGUI, KC_ENT, RAISE,  LOWER, KC_SPC, KC_RALT \
                               //`--------------------'  `--------------------'
